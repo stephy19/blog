6fdb949a3bce12e838a9c73416fbb1f3d7fa42ad
STÉPHANE IRIE <vangnybi@gmail.com>
2025-08-05 19:35:18 +0000

    Fix STATICFILES_DIRS usage for production

--- blog/settings.py
+++ blog/settings.py
@@ -139,9 +139,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_DIRS = [
-    BASE_DIR / 'static'
-]
+if os.getenv('RAILWAY_STATICFILES') != 'True':  # ou 'DEBUG' in os.environ
+    STATICFILES_DIRS = [BASE_DIR / 'static']
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
