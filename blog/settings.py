ba202e234ef2053c5b6f0391486ab237cba7b443
STÉPHANE IRIE <vangnybi@gmail.com>
2025-08-05 21:25:22 +0000

    update settings base_dir staticfiles

--- blog/settings.py
+++ blog/settings.py
@@ -63,7 +63,7 @@ MIDDLEWARE = [
 ROOT_URLCONF = 'blog.urls'
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://blog-production-da62.up.railway.app",
+    "https://blog-irie.up.railway.app/",
 ]
 
 TEMPLATES = [
