8d1d5cece48c31ae8767f0a2550863f17505b8ef
STÉPHANE IRIE <vangnybi@gmail.com>
2025-08-05 19:16:52 +0000

    update staticfiles

--- blog/urls.py
+++ blog/urls.py
@@ -16,12 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-from django.conf import settings
+from django.conf import settings, BASE_DIR
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blogapp.urls')),
     path('accounts/', include('accounts.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
+] 
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=BASE_DIR / 'static')
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
